f86d6dba949f946f64612cbf49d8fe30c0e823e8
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-06-22 21:06:25 -0400

    added /signip and /login functionality reusing past code

--- week-1/book-review-system/server/index.js
+++ week-1/book-review-system/server/index.js
@@ -75,8 +75,71 @@ app.post("/reviews", async (req, res) => {
   }
 });
 
-app.post("/signup", async (req, res) => {});
+app.post("/signup", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const foundUser = await User.findOne({ email: email });
+    if (foundUser) {
+      res.status(400).send({
+        message: "user already exists ",
+      });
+    }
 
-app.post("/login", async (req, res) => {});
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+    const isValidEmail = (email) => emailRegex.test(email);
+    const isValidPassword = (password) => password.length > 6;
+
+    if (!isValidEmail(email)) {
+      return res.status(400).send({
+        message: "email format invalid",
+      });
+    }
+
+    if (!isValidPassword(password)) {
+      return res.status(400).send({
+        message: "password must be atleast 6 characters long",
+      });
+    }
+    const newUser = await User.create(req.body);
+
+    res.status(200).send({ message: "user created ", status: "Success" });
+  } catch (error) {
+    res
+      .status(400)
+      .send({ message: "internal server error ", status: "failure" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const Userfound = await User.findOne({ email: email });
+
+    if (!Userfound) {
+      res.status(401).send({ message: "user credentials invalud" });
+    }
+    const comparedPassword = await bcrypt.compare(password, Userfound.password);
+
+    if (!comparedPassword) {
+      res.status(401).send({ message: "user credentials invalud" });
+    }
+
+    const token = jwt.sign({ email: Userfound.email }, JWT_SECRET, {
+      expiresIn: "3h",
+    });
+
+    console.log("token", token);
+    res.status(200).json({
+      token,
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).send({ message: "internal server error" });
+  }
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
